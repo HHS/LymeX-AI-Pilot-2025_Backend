120358cd8b8ee70d1c31b4e84774b6116de971e5
Vatsal <vrami@crowdplat.com>
2025-06-17 15:43:02 -0700

    added get route to fetch review programs

---
+++ src/modules/product/review_program/router.py
@@ -0,0 +1,15 @@
+from typing import Annotated, List
+from fastapi import APIRouter, Depends
+from src.modules.product.models import Product
+from src.modules.product.dependencies import get_current_product
+from src.modules.product.review_program.service import get_product_review_program
+from src.modules.product.review_program.schema import ReviewProgramResponse
+
+router = APIRouter()
+
+@router.get("/")
+async def get_product_review_program_handler(
+    product: Annotated[Product, Depends(get_current_product)],
+) -> List[ReviewProgramResponse]:
+    review_programs = await get_product_review_program(product.id)
+    return [program.to_review_program_response() for program in review_programs]
