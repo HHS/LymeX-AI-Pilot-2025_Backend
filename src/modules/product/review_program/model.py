0830de0b871c1ba1f5f8fe42560e0be638ad1d0a
Vatsal <vrami@crowdplat.com>
2025-06-17 15:42:42 -0700

    added schema for review programs

---
+++ src/modules/product/review_program/model.py
@@ -0,0 +1,16 @@
+from beanie import Document
+from typing import List
+from src.modules.product.review_program.schema import SpecialityProgram
+
+class ReviewProgram(Document):
+    productId: str
+    specialityPrograms: List[SpecialityProgram]
+
+    class Settings:
+        name = "review_programs"
+
+    def to_review_program_response(self):
+        return {
+            "productId": self.productId,
+            "specialityPrograms": self.specialityPrograms
+        }
---
+++ src/modules/product/review_program/schema.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+from typing import List
+
+class SpecialityProgram(BaseModel):
+    programName: str
+    isQualified: bool
+    reason: str
+
+class ReviewProgramResponse(BaseModel):
+    productId: str
+    specialityPrograms: List[SpecialityProgram]
