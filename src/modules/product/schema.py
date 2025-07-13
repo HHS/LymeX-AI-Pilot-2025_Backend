c6d573b8fb9e1f25a379adda669485e6ea5d867f
hanxca <hanxca@gmail.com>
2025-07-13 21:28:25 +0700

    Update

--- src/modules/product/schema.py
+++ src/modules/product/schema.py
@@ -3,6 +3,7 @@ from enum import Enum
 from typing import Literal
 from pydantic import BaseModel, Field
 
+from uuid import uuid4
 from src.modules.user.schemas import UserResponse
 
 
@@ -11,9 +12,14 @@ from src.modules.user.schemas import UserResponse
 # ============================
 
 
+def create_uuid() -> str:
+    """Generate a new UUID."""
+    return str(uuid4())
+
+
 class CreateProductRequest(BaseModel):
     name: str = Field(..., description="Product name")
-    code: str | None = Field(None, description="Product code")
+    code: str = Field(default_factory=create_uuid, description="Product code")
     model: str | None = Field(None, description="Product model")
     revision: str | None = Field(None, description="Product revision")
     category: str | None = Field(None, description="Product category")
